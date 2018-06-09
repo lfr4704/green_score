009496397fc3e22ea2b37444136b3022549a083e
Luis Robles <lfr4704@gmail.com>
2018-06-09 20:39:08 +0000

    navigation bar added

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@
 $(document).ready( () => {
     
     console.log("Ready!");
+    
+    function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 });
