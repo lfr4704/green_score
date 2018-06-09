2e0d812b04a70769ed3450ac5018b6c4f59544a7
Luis Robles <lfr4704@gmail.com>
2018-06-09 21:20:15 +0000

    revised

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,4 @@ $(document).ready( () => {
     
     console.log("Ready!");
     
-    function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
 });
