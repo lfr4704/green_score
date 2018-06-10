1f70c6fc8a545db875bb57cb91ced539f3e791d4
Luis Robles <lfr4704@gmail.com>
2018-06-10 15:22:28 +0000

    revised

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,17 @@ $(document).ready( () => {
     var myChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
-            labels: ["Energy", "Water", "Waste", "Space"],
+            labels: ["Energy", "Water", "Waste", "Space","Noise"],
             datasets: [{
                 label: 'Points',
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)'
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(37, 145, 250, 0.2)'
                 ],
-                data: [10, 20, 55, 40]
+                data: [30, 20, 55, 40, 60]
             }]
         },
 
