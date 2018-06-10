e04915c179d059cfa7cfe78037df989a68a20c3d
Luis Robles <lfr4704@gmail.com>
2018-06-10 01:00:45 +0000

    revised files

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,23 @@ $(document).ready( () => {
     
     console.log("Ready!");
     
+    var ctx = $('#mycanvas').get(0).getContext("2d");
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Energy", "Water", "Waste", "Space"],
+            datasets: [{
+                label: 'Points',
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                data: [10, 20, 55, 40]
+            }]
+        },
+
+    });
+        
 });
